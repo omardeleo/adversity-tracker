bd8f9f22e42ac187aacbbca3f6471a18ab02bc76
Omar De Leo <omar.deleo@gmail.com>
2019-07-28 21:07:25 -0400

    Create Adversities controller

---
+++ app/controllers/adversities_controller.rb
@@ -0,0 +1,30 @@
+class AdversitiesController < ApplicationController
+  def index
+    # @adversities = Adversity.where(user_id: params[:user_id])
+    @adversities = Adversity.all
+    render json: @adversities
+  end
+
+#   def create
+#     @adversity = Adversity.create(adversity_params)
+#     if @adversity.save
+#       render :show
+#     else
+#       render json: @adversity.errors.full_messages, status: 422
+#     end
+#   end
+
+#   def show
+#     @adversity = Adversity.find(params[:id])
+
+#     render :show
+#   end
+
+#   def adversity_params
+#     params.require(:adversity).permit(
+#         :user_id, 
+#         :title,
+#         :story
+#     )
+#   end
+end
