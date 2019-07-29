99d050ca03b8777451808fe084db26e9eb09720c
Omar De Leo <omar.deleo@gmail.com>
2019-07-29 00:12:55 -0400

    Prepare for deployment

---
+++ public/precache-manifest.a3e29e37d1848aeb5c4433c8e0fd5c0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48eefb952f886fc5586e24e76e8b4571",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f24d8784c47233b99753",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "f24d8784c47233b99753",
+    "url": "/static/js/main.f4f03a5f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
