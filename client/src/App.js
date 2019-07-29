32ac4525aa93f100178aaaefb520a5ea16a8ba26
Omar De Leo <omar.deleo@gmail.com>
2019-07-29 07:23:13 -0400

    Update placeholder

--- client/src/App.js
+++ client/src/App.js
@@ -10,28 +10,26 @@ class App extends React.Component {
     }
   }
   componentDidMount() {
-    fetch("api/v1/adversities")
-      .then((response) => {
-        // console.log(response.json());
-        return response.json()
-      })
-      .then(adversities => {
-        this.setState({
-          adversities: adversities
-        })
-      });
+    // fetch("api/v1/adversities")
+    //   .then((response) => {
+    //     // console.log(response.json());
+    //     return response.json()
+    //   })
+    //   .then(adversities => {
+    //     this.setState({
+    //       adversities: adversities
+    //     })
+    //   });
   }
 
   render() {
-    const adversitiesList = this.state.adversities.map((adversity, i) => {
-      return <li key={i}>{adversity.title}</li>
-    })
+    // const adversitiesList = this.state.adversities.map((adversity, i) => {
+    //   return <li key={i}>{adversity.title}</li>
+    // })
     return (
       <div>
         <h1>Adversity Tracker</h1>
-        <ul>
-          {adversitiesList}
-        </ul>
+        Welcome, user!
       </div>
     )
   }
