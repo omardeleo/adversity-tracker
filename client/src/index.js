3da110fa41b891eeb635a8f37e6b491097e28eaf
Omar De Leo <omar.deleo@gmail.com>
2019-07-30 20:39:59 -0400

    Install Redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import './index.css';
+
 import Root from './components/Root';
-// import * as serviceWorker from './serviceWorker';
+import configureStore from './store/store';
 
-ReactDOM.render(<Root />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
+document.addEventListener('DOMContentLoaded', () => {
+    let store;
+    if (window.currentUser) {
+        const preloadedState = {
+            session: { id: window.currentUser.id },
+            entities: {
+                users: { [window.currentUser.id]: window.currentUser }
+            }
+        };
+        store = configureStore(preloadedState);
+        delete window.currentUser;
+    } else {
+        store = configureStore();
+    }
+    const root = document.getElementById('root');
+    ReactDOM.render(<Root store={store} />, root);
+});
