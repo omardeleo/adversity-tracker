af9173819ba8931fd48e99a84b4df7a7d025e29f
Omar De Leo <omar.deleo@gmail.com>
2019-08-01 23:42:54 -0400

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :adversities
+      resources :users, only: [:create, :show, :update] do 
+        resources :adversities, only: [:index, :create, :show]
+      end
+      resources :feelings, only: [:index, :create, :show]
+      resource :session, only: [:create, :destroy]
     end
   end 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
