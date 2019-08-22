ee24afae5ac92010f77d93ed4c1e81810c92114f
Omar De Leo <omar.deleo@gmail.com>
2019-08-21 20:37:25 -0400

    Add action creators for updating sliders

--- client/src/actions/ui_actions.js
+++ client/src/actions/ui_actions.js
@@ -1,6 +1,10 @@
 export const CLICK_MAIN_NAV = 'CLICK_MAIN_NAV';
 export const CLICK_TITLE = 'CLICK_TITLE';
 export const CLEAR_TITLE = 'CLEAR_TITLE';
+export const UPDATE_TITLE = 'UPDATE_TITLE';
+export const UPDATE_STORY = 'UPDATE_STORY';
+export const UPDATE_FEELING_TEXT = 'UPDATE_FEELING_TEXT';
+export const UPDATE_FEELING_VALUE = 'UPDATE_FEELING_VALUE';
 
 export const setCurrentTab = tab => ({
     type: CLICK_MAIN_NAV,
@@ -15,6 +19,26 @@ export const setCurrentTitle = (payload) => ({
 export const clearTitle = () => ({
     type: CLEAR_TITLE
 });
+
+export const updateTitle = title => ({
+    type: UPDATE_TITLE,
+    title
+}) 
+
+export const updateStory = story => ({
+    type: UPDATE_STORY,
+    story
+}) 
+
+export const updateFeelingText = (feeling) => ({
+    type: UPDATE_FEELING_TEXT,
+    feeling
+}) 
+
+export const updateFeelingValue = (feeling) => ({
+    type: UPDATE_FEELING_VALUE,
+    feeling
+}) 
 
 export const setTitle = (title, id) => dispatch => {
     dispatch(setCurrentTitle({title, id}))
