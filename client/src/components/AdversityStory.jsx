8d2e6e55c4cfa28575c958e99799dd4515f498c0
Omar De Leo <omar.deleo@gmail.com>
2019-08-21 20:39:08 -0400

    Update story change event handlers

--- client/src/components/AdversityStory.jsx
+++ client/src/components/AdversityStory.jsx
@@ -38,14 +38,14 @@ export default function AdversityStory(props) {
                     "aria-label": "adversity-story"
                 }}
                 placeholder="Enter Story"
-                defaultValue=""
+                defaultValue={props.story}
                 margin="normal"
                 variant="outlined"
                 multiline={true}
                 rows="2"
                 // eslint-disable-next-line
                 inputProps={{ "aria-label": "adversity-story" }}
-                onChange={e => props.handleStory(e.target.value)}
+                onChange={e => props.handleStory({index: props.index, value: e.target.value})}
             />
         </div>
     );
