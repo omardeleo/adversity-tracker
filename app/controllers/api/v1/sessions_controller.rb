f223899bf74f372e7d95ce73c5e5877f16d82553
Omar De Leo <omar.deleo@gmail.com>
2019-09-17 00:46:02 -0400

    Add check method

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,26 +1,35 @@
 module Api::V1
     class SessionsController < ApplicationController
+        def check
+            @user = current_user
+            if @user
+                head :ok
+            else
+                render json: ["Nobody signed in"], status: 404
+            end
+        end
+
         def create
             @user = User.find_by_credentials(
-            params[:user][:email],
-            params[:user][:password]
+                params[:user][:email],
+                params[:user][:password]
             )
 
             if @user
-            login(@user)
-            render json: @user
+                login(@user)
+                render json: @user
             else
-            render json: ["Invalid email/password combination"], status: 401
+                render json: ["Invalid email/password combination"], status: 401
             end
         end
 
         def destroy
             @user = current_user
             if @user
-            logout
-            render json: @user
+                logout
+                render json: @user
             else
-            render json: ["Nobody signed in"], status: 404
+                render json: ["Nobody signed in"], status: 404
             end
         end
     end
