9ee7c00eea2490e5363ef3a3d7d9d60c6a12823c
Omar De Leo <omar.deleo@gmail.com>
2019-09-17 00:54:54 -0400

    Add logic to check for existing session

--- client/src/index.js
+++ client/src/index.js
@@ -3,22 +3,29 @@ import ReactDOM from 'react-dom';
 
 import Root from './components/Root';
 import configureStore from './store/store';
+import * as APIUtil from './util/session_api_util';
 import './index.css';
 
 document.addEventListener('DOMContentLoaded', () => {
     let store;
-    let currentUser = JSON.parse(window.localStorage.getItem('userData'));
-    if (currentUser) {
-        const preloadedState = {
-            session: { id: currentUser.id },
-            entities: {
-                users: { [currentUser.id]: currentUser }
-            }
-        };
-        store = configureStore(preloadedState);
-    } else {
-        store = configureStore();
-    }
     const root = document.getElementById('root');
-    ReactDOM.render(<Root store={store} />, root);
+    
+    APIUtil.checkSession()
+        .then(() => {
+            const currentUser = JSON.parse(window.localStorage.getItem('userData'));
+            const preloadedState = {
+                session: { id: currentUser.id },
+                entities: {
+                    users: { [currentUser.id]: currentUser }
+                }
+            };
+            store = configureStore(preloadedState);
+            ReactDOM.render(<Root store={store} />, root);
+            
+        })
+        .catch(() => {
+            window.localStorage.clear();
+            store = configureStore();
+            ReactDOM.render(<Root store={store} />, root);
+        }) 
 });
