c623035220e94e17f70cbab6da0f4592a005e039
Omar De Leo <omar.deleo@gmail.com>
2019-09-23 23:29:20 -0400

    Create displayMonth util

--- client/src/util/date_util.js
+++ client/src/util/date_util.js
@@ -1,11 +1,14 @@
-export default function getDateRange() {
-    let currDate = new Date();
-    let currYear = currDate.getFullYear();
-    let currMonth = currDate.getMonth();
+let currDate = new Date();
+let currYear = currDate.getFullYear();
+let currMonth = currDate.getMonth();
 
+export const getDateRange = () => {
     return {
         start: new Date(Date.UTC(currYear, currMonth)).valueOf(),
         end: new Date(Date.UTC(currYear, currMonth + 1, 0, 23, 59, 59, 999)).valueOf()
     }
+}
 
+export const displayMonth = () => {
+    return currDate.toLocaleString('default', { month: 'long', year: 'numeric' });
 }
