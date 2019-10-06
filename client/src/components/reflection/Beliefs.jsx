8a9bf94b2e91fcdd7959d9940a1d24a381f5f08a
Mark Rodriguez <mrkxvncnt@gmail.com>
2019-10-05 18:06:25 -0700

    Import customSlider to Beliefs tracker

--- client/src/components/reflection/Beliefs.jsx
+++ client/src/components/reflection/Beliefs.jsx
@@ -1,11 +1,23 @@
 import React, { useState } from "react";
+import BeliefsSlider from "../CustomSlider";
+import { useStyles } from '../../util/ui_util';
 import '../stylesheets/Reflection.css';
 
 export const Beliefs = () => {
   const [belief, setBelief] = useState('Beliefs');
+  const classes = useStyles();
+  const sliderVal = 1;
+  const index = 1;
+
   return (
     <div className="beliefs">
       <div className='module-label'>Beliefs</div>
+      <BeliefsSlider
+        sliderVal={sliderVal}
+        // handleChange={props.handleSliderChange}
+        className={classes.slider}
+        index={index}
+      />
     </div>
   );
 };
