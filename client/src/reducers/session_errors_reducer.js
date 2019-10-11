38f3dcd1bfe739236203a0db00ca8ab0b2c30e37
Omar De Leo <omar.deleo@gmail.com>
2019-10-10 23:15:43 -0400

    Update session errors object shape

--- client/src/reducers/session_errors_reducer.js
+++ client/src/reducers/session_errors_reducer.js
@@ -8,7 +8,28 @@ export default (state = [], action) => {
     Object.freeze(state);
     switch (action.type) {
         case RECEIVE_SESSION_ERRORS:
-            return action.errors;
+            const errors = action.errors;
+            const newState = {
+                name: null,
+                email: null,
+                password: null
+            }
+            if (errors.includes("Name can't be blank")) {
+                newState.name = "Name can't be blank"
+            } 
+            if (errors.includes("Email Invalid email")) {
+                newState.email = "Invalid email"
+            } 
+            if (errors.includes("Email can't be blank")) {
+                newState.email = "Email can't be blank"
+            } 
+            if (errors.includes("Password is too short (minimum is 6 characters)")) {
+                newState.password = "Password is too short (minimum is 6 characters)"
+            }
+            if (errors.includes("Invalid email/password combination")) {
+                newState.signIn = "Invalid email/password combination"
+            }
+            return newState;
         case RECEIVE_CURRENT_USER:
             return [];
         case CLEAR_SESSION_ERRORS:
