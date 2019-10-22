24e616fed5c8f0601d2e288021124e21db7b420a
Omar De Leo <omar.deleo@gmail.com>
2019-10-21 21:47:51 -0400

    Adjust graph date range so that most recently added data point doesn't get cut off

--- client/src/util/date_util.js
+++ client/src/util/date_util.js
@@ -2,8 +2,8 @@ let currentDate = new Date();
 let [thisMonth, today, thisYear] = [currentDate.getMonth(),
     currentDate.getDate(),
     currentDate.getFullYear()];
-let startDate = new Date(Date.UTC(thisYear, thisMonth, today - 30)).valueOf();
-let endDate = new Date(Date.UTC(thisYear, thisMonth, today + 1)).valueOf();
+let startDate = new Date(Date.UTC(thisYear, thisMonth, today - 29)).valueOf();
+let endDate = new Date(Date.UTC(thisYear, thisMonth, today + 2)).valueOf();
 
 export const getDateRange = () => {
     return {
