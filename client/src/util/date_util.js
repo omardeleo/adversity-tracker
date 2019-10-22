91860e72ae354b5e59923bde49a01ac6100cab13
Omar De Leo <omar.deleo@gmail.com>
2019-10-21 22:03:34 -0400

    Update date range start date to 30 days ago

--- client/src/util/date_util.js
+++ client/src/util/date_util.js
@@ -2,7 +2,7 @@ let currentDate = new Date();
 let [thisMonth, today, thisYear] = [currentDate.getMonth(),
     currentDate.getDate(),
     currentDate.getFullYear()];
-let startDate = new Date(Date.UTC(thisYear, thisMonth, today - 29)).valueOf();
+let startDate = new Date(Date.UTC(thisYear, thisMonth, today - 30)).valueOf();
 let endDate = new Date(Date.UTC(thisYear, thisMonth, today + 2)).valueOf();
 
 export const getDateRange = () => {
