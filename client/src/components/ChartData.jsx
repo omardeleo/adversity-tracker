accc6c05a70c34ae9689868230f2dddb294cb3e0
Omar De Leo <omar.deleo@gmail.com>
2019-10-28 17:44:55 -0400

    Pass stories down to graph

--- client/src/components/ChartData.jsx
+++ client/src/components/ChartData.jsx
@@ -14,28 +14,34 @@ function Placeholder() {
 class ChartData extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {loadChart: false}
+        this.state = {
+            loadChart: false
+        }
     }
 
-    componentDidMount() {     
+    componentDidMount() {
         this.props.fetchAdversities(this.props.currentUser.id);
     }
 
     render() {
         const { adversities } = this.props;
+        console.log('ChartData.jsx', adversities)
         const adversityData = [];
         for (let adversity of adversities) {
             let recognitions = [];
             for (let recognition of adversity.recognitions) {
-                recognitions.push(recognition.created_at) 
+                // console.log('recognnn CHartDate.jsx', recognition)
+                // let trunc_story = recognition.story.length > 120 ? recognition.story.slice(0, 120) + "..." : recognition.story;
+                // recognitions.push({timestamp: recognition.created_at, story: trunc_story}) 
+                recognitions.push({ timestamp: recognition.created_at, story: recognition.story })
             }
-            let point = { adv_id: adversity.id, title: adversity.title, recognitions: recognitions }
+            let point = { adv_id: adversity.id, title: adversity.title, recognitions }
             adversityData.push(point);
         }
-        let chartDiv = adversityData.length > 0 ? 
-            <Chart 
-                adversities={adversityData} 
-                onTitleClick={this.props.setTitle} 
+        let chartDiv = adversityData.length > 0 ?
+            <Chart
+                adversities={adversityData}
+                onTitleClick={this.props.setTitle}
             /> : <Placeholder />;
         return chartDiv;
     }
