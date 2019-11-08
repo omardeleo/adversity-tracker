52b1311ebaed38fe632f0dda9fe334a5862fc39c
Omar De Leo <omar.deleo@gmail.com>
2019-11-08 12:14:24 -0500

    Map numeric feeling intensity value to intensity adverb

--- client/src/components/ChartData.jsx
+++ client/src/components/ChartData.jsx
@@ -22,17 +22,37 @@ class ChartData extends React.Component {
     componentDidMount() {
         this.props.fetchAdversities(this.props.currentUser.id);
     }
+    
+
+    convertIntensityValue(value) {
+        if (value < 34) {
+            return "mildly";
+        } else if (value > 34 && value < 67) {
+            return "moderately";
+        } else {
+            return "intensely";
+        }
+    }
+
+    mapIntensityValues(feelings) {
+        feelings.forEach(feeling => {
+            feeling.intensity = this.convertIntensityValue(feeling.intensity);
+        })
+        return feelings;
+    }
 
     render() {
         const { adversities } = this.props;
         const adversityData = [];
+        
         for (let adversity of adversities) {
             let recognitions = [];
             for (let recognition of adversity.recognitions) {
+                let { feelings, story } = recognition;
                 recognitions.push({ 
                     timestamp: recognition.created_at, 
-                    story: recognition.story, 
-                    feelings: recognition.feelings 
+                    story, 
+                    feelings: this.mapIntensityValues(feelings)
                 })
             }
             let point = { adv_id: adversity.id, title: adversity.title, recognitions }
