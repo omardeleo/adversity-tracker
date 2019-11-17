27ddf912ae9ded5d8ae5af4c19fe0da9f4f9a093
Mark Rodriguez <mrkxvncnt@gmail.com>
2019-11-17 15:59:03 -0800

    Implement modal actions

--- client/src/reducers/root_reducer.js
+++ client/src/reducers/root_reducer.js
@@ -5,5 +5,6 @@ import session from './session_reducer';
 import errors from './errors_reducer';
 import ui from './ui_reducer';
 import form from './form_reducer';
+import modal from './modal_reducer';
 
-export default combineReducers({ entities, form, session, ui, errors });
+export default combineReducers({ entities, form, session, ui, modal, errors });
