44c4bdd473b1fcbd43612c1b706e51efec6c6dec
Omar De Leo <omar.deleo@gmail.com>
2019-11-21 15:09:19 -0500

    Resolve merge conflict

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2019_11_19_194201) do
-=======
 ActiveRecord::Schema.define(version: 2019_11_19_025242) do
->>>>>>> master
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
