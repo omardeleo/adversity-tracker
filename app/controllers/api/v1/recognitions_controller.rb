d5a48d462e8351af5260e1d1f6e86d247e04f5e0
Omar De Leo <omar.deleo@gmail.com>
2019-12-09 17:45:13 -0500

    Add recognition_date to permitted params

--- app/controllers/api/v1/recognitions_controller.rb
+++ app/controllers/api/v1/recognitions_controller.rb
@@ -5,14 +5,14 @@ module Api::V1
         def create
             @recognition = Recognition.new(recognition_params)
             if @recognition.save
-            render json: @recognition
+                render json: @recognition
             else
-            render json: @recognition.errors.full_messages, status: 422
+                render json: @recognition.errors.full_messages, status: 422
             end
         end
 
         def recognition_params
-            params.require(:recognition).permit(:adversity_id, :story)
+            params.require(:recognition).permit(:adversity_id, :story, :recognition_date)
         end
     end
 end
