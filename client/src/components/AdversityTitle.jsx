0de50c3f454d6b8960fcaeda90f6cf59aec60bf9
Omar De Leo <omar.deleo@gmail.com>
2019-12-09 17:51:52 -0500

    Add input for past date

--- client/src/components/AdversityTitle.jsx
+++ client/src/components/AdversityTitle.jsx
@@ -42,6 +42,8 @@ export default function AdversityTitle(props) {
                 variant="outlined"
                 onChange={e => props.handleTitle(e.target.value)}
             />
+            <input id="past-date" placeholder="ex: 12/13/2017" />
+            <label for="past-date">Past adversity date</label>
         </div>
     );
 }
