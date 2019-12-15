3a8176668a8f90fb88912bf486334893c8b40a1f
Omar De Leo <omar.deleo@gmail.com>
2019-12-14 21:34:07 -0500

    Remove debugger

--- client/src/util/recognition_api_util.js
+++ client/src/util/recognition_api_util.js
@@ -1,7 +1,6 @@
 import $ from 'jquery';
 
 export const createRecognition = (adversity_id, story, recognition_date) => {
-    debugger
     return $.ajax({
         method: 'POST',
         url: `/api/v1/recognitions`,
